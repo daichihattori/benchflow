bbfd08a03ff547cf7ae53b3d5f33861d37bf063f
daichihattori <59868500+daichihattori@users.noreply.github.com>
2025-10-06 23:12:17 +0900

    fixed bench

---
+++ conftest.py
@@ -0,0 +1,24 @@
+# conftest.py
+def pytest_benchmark_generate_json(
+    config, benchmarks, include_data, machine_info, commit_info
+):
+    results = []
+    for b in benchmarks:
+        stats = b["stats"]
+        results.append(
+            {
+                "name": b["name"],
+                "min": stats.min,
+                "max": stats.max,
+                "mean": stats.mean,
+                "stddev": stats.stddev,
+                "rounds": stats.rounds,
+            }
+        )
+
+    return {
+        "machine_info": {
+            "processor": machine_info.get("processor"),
+        },
+        "benchmarks": results,
+    }
