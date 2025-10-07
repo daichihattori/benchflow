012bff6d8a6a6dfbdee95af9ee936ccfd6fe8456
daichihattori <59868500+daichihattori@users.noreply.github.com>
2025-10-07 15:51:34 +0900

    made worker

--- src/fib.py
+++ src/fib.py
@@ -1,4 +1,15 @@
+from multiprocessing import Pool
+
+
+def fib_worker(n):
+    if n < 2:
+        return n
+    return fib_worker(n - 1) + fib_worker(n - 2)
+
+
 def fib(n: int) -> int:
     if n < 2:
         return n
-    return fib(n - 1) + fib(n - 2)
+    with Pool(2) as p:
+        a, b = p.map(fib_worker, [n - 1, n - 2])
+    return a + b
